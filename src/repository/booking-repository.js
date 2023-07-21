88662539ff7302e2787f2a36bc4cd23d7f868729
kenil-kanani <kenilkananigecbvn@gmail.com>
2023-07-21 14:58:24 +0530

    Completed create booking

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -9,13 +9,29 @@ class BookingRepository {
             const booking = await Booking.create(data);
             return booking;
         } catch (error) {
-            if(error.name == 'SequelizeValidationError') {
+            if (error.name == 'SequelizeValidationError') {
                 throw new ValidationError(error);
             }
             throw new AppError(
-                'RepositoryError', 
-                'Cannot create Booking', 
+                'RepositoryError',
+                'Cannot create Booking',
                 'There was some issue creating the booking, please try again later',
+                StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+    }
+    async update(bookingId, data) {
+        try {
+            const booking = await Booking.findByPk(bookingId);
+            if (data.status) {
+                booking.status = data.status;
+            }
+            await booking.save();
+            return booking;
+        } catch (error) {
+            throw new AppError(
+                'RepositoryError',
+                'Cannot update Booking',
+                'There was some issue updating the booking, please try again later',
                 StatusCodes.INTERNAL_SERVER_ERROR);
         }
     }
